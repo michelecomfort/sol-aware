f6a0c3da37c2300db8aeb53075be937a5a126b31
Michele Comfort <michelecomfort@gmail.com>
2022-01-13 22:28:00 -0700

    Build out of landing page and form input

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,16 +3,16 @@ import '../Styles/Header.css'
 
 const Header = () => {
     return (
-        <header>
+        <header className='header'>
+            <img src={require('../assets/sol-aware-heading.png')}className='title-logo'/>
             <nav>
                 <ul className='nav-bar'>
-                    <li className='nav-item'>home</li>
+                    <li className='nav-item home'>home</li>
                     <li className='nav-item'>uv guide</li>
                     <li className='nav-item'>skin types</li>
                 </ul>
             </nav>
-            <img src={require('../assets/sol-aware-title.png')} className='title-logo'/>
-            <p className='empower-text'>empowering you to care for the health of your skin</p>
+            
         </header>
     )
 }
--- src/Components/ZipInput.js
+++ src/Components/ZipInput.js
@@ -38,13 +38,32 @@ class ZipInput extends Component {
 
     render() {
         return (
-            <form>
-                <label className='zipcode'>
-                    <input className='zipcode-input' placeholder='Zip Code' onChange={(e) => this.handleChange(e)}/>
-                </label>
-                <div className='button-container'>
-                    <button className='go-button' onClick={(e) => this.submitZipcode(e)}>Go</button>
+            <form className='zipcode-form'>
+                <div className='input-section'>
+                    <h2 className='slogan'>skin health starts with sun knowledge</h2>
+                    <div className='input-button'>
+                        <label className='zipcode'>
+                            <input className='zipcode-input' placeholder='Zip Code' onChange={(e) => this.handleChange(e)}/>
+                        </label>
+                        <div className='button-container'>
+                            <button type='button'className='go-button' onSubmit={(e) => this.submitZipcode(e)} >Go</button>
+                        </div>
+                    </div>
                 </div>
+                <aside className='small-grid'>
+                    <div className='small-info-section'>
+                        <p className='small-text'>The health of our skin starts with decisions we make everyday</p>
+                        <img src={require('../assets/sunflower-woman.jpeg')}/>
+                    </div>
+                    <div className='small-info-section'>
+                        <p className='small-text'>Understand the truth about SPF protection and its correct usage</p>
+                        <img src={require('../assets/sunscreen-woman.jpeg')}/>
+                    </div>
+                    <div className='small-info-section'>
+                        <p className='small-text'>Protect yourself and loved ones for melanoma and skin disease</p>
+                        <img src={require('../assets/generational.jpeg')}/>
+                    </div>
+                </aside>
             </form>
         )
     }
