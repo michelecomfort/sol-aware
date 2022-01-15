a3a6ea5fdd73b6ac0dc85f10a8742e386c78292d
Michele Comfort <michelecomfort@gmail.com>
2022-01-14 23:46:16 -0700

    Add elements to UvGuide and SkinTypes components

--- src/Components/SkinTypes.js
+++ src/Components/SkinTypes.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import '../Styles/SkinTypes.css'
 
 const SkinTypes = () => {
     return (
-        <div>
-            <img/>
-            <img/>
+        <div className='skin-type-section'>
+            <h2>Determing your Skin Type</h2>
+            <div className='skin-type-images'>
+                <img src={require('../assets/skin-types.png')} className='skin-type-image'/>
+                <img src={require('../assets/smiling-woman.jpeg')} className='smiling-woman-image'/>
+            </div>
         </div>
     )
 }
